9023fc9363b0cc2e68507ab46e50aacb4ec8fc38
Nethravathi T <59238431+Nethravathi1997@users.noreply.github.com>
2023-06-27 19:01:44 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,11 @@
-
+import { useState } from "react";
+import "./App.scss";
+import Topbar from "./components/topbar/Topbar";
+import Menu from "./components/menu/menu";
+import Intro from "./components/intro/intro";
+import Portfolio from "./components/portfolio/portfolio";
+import Projects from "./components/projects/projects";
+import Contact from "./components/contact/contact";
 
 
 function App() {
