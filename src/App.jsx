3e11abd8c79eca3dcbb581dd6b3370ed3f218dbe
Nethravathi T <59238431+Nethravathi1997@users.noreply.github.com>
2023-06-27 19:02:02 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,21 +8,7 @@ import Projects from "./components/projects/projects";
 import Contact from "./components/contact/contact";
 
 
-function App() {
-   const [menuOpen,setMenuOpen] = useState(false);
-  return (
-    <div className="App">
-     <Topbar menuOpen = {menuOpen} setMenuOpen={setMenuOpen}/>
-     <Menu  menuOpen = {menuOpen} setMenuOpen={setMenuOpen} />
 
-     <div className="sections">
-     <Intro />
-     <Portfolio />
-     <Projects />
-     <Contact />
-     </div>
-    </div>
-  );
 }
 
 export default App;
